240125d99ed3ae783e5e1e99d1bfa539f4434aab
glasswaster <whubler@gmail.com>
2015-04-07 00:51:29 -0700

    first run

---
+++ desktopButton/frmTracker.Designer.vb
@@ -0,0 +1,84 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmTracker
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.cmdStart = New System.Windows.Forms.Button()
+        Me.lblSecondsLeft = New System.Windows.Forms.Label()
+        Me.lblParticipants = New System.Windows.Forms.Label()
+        Me.niCountDown = New System.Windows.Forms.NotifyIcon(Me.components)
+        Me.SuspendLayout()
+        '
+        'cmdStart
+        '
+        Me.cmdStart.Location = New System.Drawing.Point(197, 226)
+        Me.cmdStart.Name = "cmdStart"
+        Me.cmdStart.Size = New System.Drawing.Size(75, 23)
+        Me.cmdStart.TabIndex = 0
+        Me.cmdStart.Text = "Start"
+        Me.cmdStart.UseVisualStyleBackColor = True
+        '
+        'lblSecondsLeft
+        '
+        Me.lblSecondsLeft.AutoSize = True
+        Me.lblSecondsLeft.Font = New System.Drawing.Font("Microsoft Sans Serif", 80.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblSecondsLeft.Location = New System.Drawing.Point(57, 24)
+        Me.lblSecondsLeft.Name = "lblSecondsLeft"
+        Me.lblSecondsLeft.Size = New System.Drawing.Size(170, 120)
+        Me.lblSecondsLeft.TabIndex = 1
+        Me.lblSecondsLeft.Text = "##"
+        '
+        'lblParticipants
+        '
+        Me.lblParticipants.AutoSize = True
+        Me.lblParticipants.Location = New System.Drawing.Point(114, 172)
+        Me.lblParticipants.Name = "lblParticipants"
+        Me.lblParticipants.Size = New System.Drawing.Size(56, 13)
+        Me.lblParticipants.TabIndex = 2
+        Me.lblParticipants.Text = "#######"
+        '
+        'niCountDown
+        '
+        Me.niCountDown.Text = "/r/theButton"
+        Me.niCountDown.Visible = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.lblParticipants)
+        Me.Controls.Add(Me.lblSecondsLeft)
+        Me.Controls.Add(Me.cmdStart)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents cmdStart As System.Windows.Forms.Button
+    Friend WithEvents lblSecondsLeft As System.Windows.Forms.Label
+    Friend WithEvents lblParticipants As System.Windows.Forms.Label
+    Friend WithEvents niCountDown As System.Windows.Forms.NotifyIcon
+
+End Class
---
+++ desktopButton/frmTracker.vb
@@ -0,0 +1,88 @@
+﻿
+Imports System.Net.WebSockets
+Imports System.Threading
+Imports System.Text.RegularExpressions
+Imports System.Text
+Imports Newtonsoft.Json.Linq
+
+Public Class frmTracker
+    'I'm using this in the createTextIcon sub to releases all of the resources that the icon/icons would have used.
+    Public Declare Function DestroyIcon Lib "user32.dll" (ByVal hIcon As Int32) As Int32
+
+    Private wsServer As Uri
+    Dim fontToUse As Font = New Font("Microsoft Sans Serif", 14, FontStyle.Regular, GraphicsUnit.Pixel)
+    Dim brushToUse As Brush
+    Dim bitmapText As Bitmap = New Bitmap(16, 16)
+    Dim g As Graphics = Drawing.Graphics.FromImage(bitmapText)
+    Dim hIcon As IntPtr
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim request As System.Net.HttpWebRequest = System.Net.HttpWebRequest.Create("http://www.reddit.com/r/thebutton")
+        Dim response As System.Net.HttpWebResponse = request.GetResponse()
+        Dim sr As System.IO.StreamReader = New System.IO.StreamReader(response.GetResponseStream())
+        Dim sourcecode As String = sr.ReadToEnd()
+        Dim pattern As String = "wss:\/\/(.+?)"""
+        Dim m As Match = Regex.Match(sourcecode, pattern)
+        wsServer = New Uri(m.Value.Trim(""""))
+
+        niCountDown.Icon = New Icon("..\..\app.ico")
+    End Sub
+
+    Private Async Sub start()
+        Dim secs As Integer
+        Dim clr As Color
+        Using ws As New ClientWebSocket()
+            Dim result As WebSocketReceiveResult
+
+            Await ws.ConnectAsync(wsServer, CancellationToken.None)
+
+            While True
+                Dim bytesReceived As New ArraySegment(Of Byte)(New Byte(1023) {})
+                result = Await ws.ReceiveAsync(bytesReceived, CancellationToken.None)
+                Dim s As String = Encoding.UTF8.GetString(bytesReceived.Array)
+                Dim json As JObject = JObject.Parse(s)
+                secs = CInt(json.SelectToken("payload").SelectToken("seconds_left"))
+
+                Select Case secs
+                    Case Is >= 52
+                        clr = Color.Purple
+                    Case Is >= 42
+                        clr = Color.Blue
+                    Case Is >= 32
+                        clr = Color.Green
+                    Case Is >= 22
+                        clr = Color.Yellow
+                    Case Is >= 12
+                        clr = Color.Orange
+                    Case Is >= 1
+                        clr = Color.Red
+                    Case 0
+                        clr = Color.White
+                End Select
+
+                lblSecondsLeft.ForeColor = clr
+                lblSecondsLeft.Text = secs.ToString
+                lblParticipants.Text = json.SelectToken("payload").SelectToken("participants_text")
+
+                createTextIcon(secs, clr)
+            End While
+        End Using
+    End Sub
+
+    Sub createTextIcon(seconds As Integer, clr As Color)
+        Try
+            g.Clear(Color.White)
+            brushToUse = New SolidBrush(clr)
+            g.DrawString(seconds.ToString, fontToUse, brushToUse, -2, -1)
+            hIcon = (bitmapText.GetHicon)
+            niCountDown.Icon = Drawing.Icon.FromHandle(hIcon)
+            DestroyIcon(hIcon.ToInt32)
+        Catch exc As Exception
+            MessageBox.Show(exc.InnerException.ToString, "Somethings not right?", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+
+    Private Sub Form1_Shown(sender As Object, e As EventArgs) Handles Me.Shown
+        start()
+    End Sub
+End Class
